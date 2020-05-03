e24899f6c62dbe85f67fba4680efb5c13bb78b26
Adi Muraru <amuraru@adobe.com>
2020-05-03 13:26:20 +0300

    Upgrade to zookeeper 3.6.1

--- docker/zu/build.gradle.kts
+++ docker/zu/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.apache.zookeeper:zookeeper:3.6.0")
+    implementation("org.apache.zookeeper:zookeeper:3.6.1")
 }
 
 tasks.withType<ShadowJar>() {
